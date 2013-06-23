97840844925251675c31671ab7ebf14228450613
Jeff Taggart <jetaggart@gmail.com>
2013-06-23 19:18:55 +0100

    Added basic auth to staging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :basic_authentication 
+
+  protected
+  
+  def basic_authentication
+    return unless Rails.env.staging?
+
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["STAGING_USERNAME"] && password == ENV["STAGING_PASSWORD"]
+    end
+  end
 end
---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,51 @@
+require 'spec_helper'
+
+class TestController < ApplicationController
+  def some_route
+    render nothing: true
+  end
+end
+
+NairobiDevSchool::Application.routes.draw do
+  get 'some_route' => "test#some_route"
+end
+
+describe TestController do
+  describe "authentication" do
+    before :each do
+      @old_env = Rails.env
+      Rails.env = env
+    end
+
+    after :each do
+      Rails.env = @old_env
+    end
+
+    context "production" do
+      let(:env) { "production" }
+
+      it "doesn't require auth" do
+        get :some_route 
+        expect(response).to be_success
+      end
+    end
+
+    context "development" do
+      let(:env) { "development" }
+
+      it "doesn't require authentication" do
+        get :some_route
+        expect(response).to be_success
+      end
+    end
+
+    context "staging" do
+      let(:env) { "staging" }
+      
+      it "requires basic auth" do
+        get :some_route
+        expect(response).to_not be_success
+      end
+    end
+  end
+end
