1c860ac652e379f09a214f5f6f32600e7f1aaad4
Victor Miclovich Amooti <vicmiclovich@gmail.com>
2013-07-04 15:22:22 +0300

    admin users should not be signing up just like anyone

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,4 +1,5 @@
 class Admin < ActiveRecord::Base
-  devise :database_authenticatable, :registerable,
+  # removed registerable; anyone can register as admin if this is left here.
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 end
