35c71622c7197305136d851a85000b5c69c62812
Jeff Taggart <jetaggart@gmail.com>
2013-07-05 01:07:32 +0100

    fix tabs

--- app/controllers/base_applications_controller.rb
+++ app/controllers/base_applications_controller.rb
@@ -5,20 +5,20 @@ class BaseApplicationsController < ApplicationController
 	end
 
 	def new
-    set_model(model_class.new)
+		set_model(model_class.new)
 	end
 
-  def create
-    set_model(model_class.new(application_params))
+	def create
+		set_model(model_class.new(application_params))
 
-    if model.save
-      flash[:success] = I18n.t("applications.create.success")
-      redirect_to root_path
-    else
-      flash[:error] = I18n.t("applications.create.failure")
-      render :new
-    end
-  end
+		if model.save
+			flash[:success] = I18n.t("applications.create.success")
+			redirect_to root_path
+		else
+			flash[:error] = I18n.t("applications.create.failure")
+			render :new
+		end
+	end
 
 	private
 
@@ -38,8 +38,8 @@ class BaseApplicationsController < ApplicationController
 		self.class.instance_variable_get(:@model_class)	
 	end
 
-  def application_params
-    params.require(model_name)
-          .permit(*model_class.const_get("REQUIRED_ATTRS"))
-  end
+	def application_params
+		params.require(model_name)
+		.permit(*model_class.const_get("REQUIRED_ATTRS"))
+	end
 end
