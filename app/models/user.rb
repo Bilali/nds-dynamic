f9609f02cd707fafbb0227d9ab66b52e60177190
Victor Miclovich Amooti <vicmiclovich@gmail.com>
2013-07-29 14:31:25 +0300

    added rolify

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  rolify
   # removed registerable; anyone can register as admin if this is left here.
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
