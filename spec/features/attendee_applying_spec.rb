5054e4cd4de69523a4634aa610cf7a45a1cd3d30
Victor Miclovich Amooti <vicmiclovich@gmail.com>
2013-07-29 16:10:44 +0300

    added 2 more fields

--- spec/features/attendee_applying_spec.rb
+++ spec/features/attendee_applying_spec.rb
@@ -11,6 +11,8 @@ describe "applying to be a attendee" do
     fill_in "Name", with: "Njeri Chelimo"
     fill_in "Email", with: "dev@nds.org"
     fill_in "Reason for applying", with: "Cause NDS is awesome!"
+    fill_in "Youtube or Vimeo link", with: "http://bit.ly/2343"
+    fill_in "Github or Bitbucket link/profile", with: "http://bit.ly/2234"
 
     click_on "Apply"
 
