a247294f1e665ae7d61e4440e6143436c4b45c6c
Victor Miclovich Amooti <vicmiclovich@gmail.com>
2013-09-04 18:23:43 +0300

    Clean route to login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 NairobiDevSchool::Application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new', as: 'login'
+    get 'logout', to: 'devise/sessions#destroy', as: 'logout'
+  end
+
   root to: "home#index"
 
   namespace :admin do
