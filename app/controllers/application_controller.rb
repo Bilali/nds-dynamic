f2fb97cb4428349728f59d626b44808cd487c98b
Jeff Taggart <jeff@jetaggart.com>
2013-12-23 11:30:14 +0000

    Remove basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_filter :basic_authentication
-
   protected
 
   def after_sign_in_path_for(resource_or_scope)
@@ -10,14 +8,6 @@ class ApplicationController < ActionController::Base
       admin_attendee_applications_path
     else
       root_path
-    end
-  end
-
-  def basic_authentication
-    return if Rails.env.development? || Rails.env.test?
-
-    authenticate_or_request_with_http_basic do |username, password|
-      username == "nds" && password == "nairobi_dev_school!"
     end
   end
 end
