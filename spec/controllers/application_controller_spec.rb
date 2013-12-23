f638d900cf7e370d94c440d96797a8d9c9de5084
Jeff Taggart <jeff@jetaggart.com>
2013-12-23 12:17:02 +0000

    Tweaks to setences and removal of Supporters

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -17,41 +17,4 @@ describe TestController do
     Rails.application.reload_routes!
   end
 
-  describe "authentication" do
-    before :each do
-      @old_env = Rails.env
-      Rails.env = env
-    end
-
-    after :each do
-      Rails.env = @old_env
-    end
-
-    context "production" do
-      let(:env) { "production" }
-
-      it "requires auth" do
-        get :some_route 
-        expect(response).to_not be_success
-      end
-    end
-
-    context "development" do
-      let(:env) { "development" }
-
-      it "doesn't require authentication" do
-        get :some_route
-        expect(response).to be_success
-      end
-    end
-
-    context "staging" do
-      let(:env) { "staging" }
-      
-      it "requires basic auth" do
-        get :some_route
-        expect(response).to_not be_success
-      end
-    end
-  end
 end
