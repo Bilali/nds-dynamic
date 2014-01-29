230936bfd3c353481c0ca07516a0bf115408928c
Jeff Taggart <jeff@jetaggart.com>
2014-01-29 22:44:41 +0000

    fix attendee applications

--- app/models/attendee_application.rb
+++ app/models/attendee_application.rb
@@ -1,6 +1,8 @@
 class AttendeeApplication < Application
   REQUIRED_ATTRS = [:name, :email, :reason_for_applying]
-  ADDITIONAL_ATTRS = [:twitter, :what_to_do_after_nds, :github_or_bitbucket_profile]
+  ADDITIONAL_ATTRS = [:twitter, :what_to_do_after_nds,
+                      :github_or_bitbucket_profile, :can_pay_fees,
+                      :reason_for_sponsorship]
 
   validates(*REQUIRED_ATTRS, :presence => true)
 
