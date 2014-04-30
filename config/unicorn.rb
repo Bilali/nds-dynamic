ab5bba149438852fed22a1de899e5496f3c2aa89
Jeff Taggart <jeff@jetaggart.com>
2014-04-29 20:28:49 -0600

    Tweak unicorn for heroku

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,8 +1,4 @@
-app_dir = "/home/deploy/nds/current"
-
 worker_processes 3
-working_directory app_dir
-
 timeout 15
 preload_app true
 
@@ -20,7 +16,7 @@ before_fork do |server, worker|
 
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.connection.disconnect!
-end 
+end
 
 after_fork do |server, worker|
   Signal.trap 'TERM' do
